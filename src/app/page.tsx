4ce49639564a18d3e3ded67cf39c872c52c977c5
SadiaMs <162547312+SadiaMs@users.noreply.github.com>
2025-01-13 14:19:22 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/Hero";
-import Image from "next/image";
+
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
